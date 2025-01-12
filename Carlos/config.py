cd8574ffa9dccc9662f7303bb464e6a4882b5adc
Carlos Eduardo <142277959+Carlosomr@users.noreply.github.com>
2025-01-12 11:28:33 -0300

    Add files via upload

--- Carlos/config.py
+++ Carlos/config.py
@@ -1,4 +1,14 @@
+import os
+from urllib.parse import quote_plus
+
 class Config:
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    DATABASE_USER = os.getenv('DATABASE_USER', 'postgres')
+    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'Suporte0!@#$%¨&*(')
+    DATABASE_HOST = os.getenv('DATABASE_HOST', 'indistinctly-infinite-narwhal.data-1.use1.tembo.io')
+    DATABASE_PORT = os.getenv('DATABASE_PORT', '5432')
+    DATABASE_NAME = os.getenv('DATABASE_NAME', 'postgres')
+
+    DATABASE_URI = f"postgresql://{DATABASE_USER}:{quote_plus(DATABASE_PASSWORD)}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI
     SQLALCHEMY_TRACK_MODIFICATIONS = False
